441d4b4cd411f13564a8770c4d68498a9fc39203
sjbabadi <sheila.babadi@yahoo.com>
2021-06-27 14:56:37 -0400

    add database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,6 +18,9 @@ func main() {
 	err := godotenv.Load()
 	app.check(err)
 
+	//db, err := app.openDB()
+	app.check(err)
+
 	port := os.Getenv("PORT")
 	portStr := fmt.Sprintf(":%s", port)
 
@@ -24,4 +28,30 @@ func main() {
 
 	err = http.ListenAndServe(portStr, app.routes())
 	app.check(err)
+}
+
+func (app *application) getConnectionString() string {
+	host := os.Getenv("PG_HOST")
+	port := os.Getenv("PG_PORT")
+	user := os.Getenv("PG_USER")
+	// CONFIG: add pw if your DB user has a password
+	//	pw := os.Getenv("PG_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s "+
+		" sslmode=disable", host, port, user, dbName)
+
+	return connStr
+}
+
+func (app *application) openDB() (*sql.DB, error) {
+	connStr := app.getConnectionString()
+
+	db, err := sql.Open("postgres", connStr)
+	app.check(err)
+
+	err = db.Ping()
+	app.check(err)
+
+	return db, nil
 }
